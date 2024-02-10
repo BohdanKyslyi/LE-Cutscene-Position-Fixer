af8c8952bebfda33442f2ebc164e32771d6309b7
BohdanKyslyi <bohdan.kyslyi.13@gmail.com>
2024-02-11 00:34:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LE-Cutscene-Position-Fixer
+ When you create cutscenes in the X-Ray SDK Level Editor, while the camera is moving, it can change the sign of its position, so instead of smoothly rotating the camera along the Y axis, we get a 360° turn. This program was created to fix this situation, and now you can enter the problematic "turned" Y position of the camera, and get the corrected one
---
+++ Source Code/LECutscenePositionFixer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LECutscenePositionFixer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str =
+            {
+                "Enter your ",
+                "Your new ",
+                "position: ",
+                "Помилка ",
+                "\n\nСкорiше за все, Ви помилилися пiд час написання позицiї. Переконайтеся, що використовуєте коми, а не крапки + не пишiть туди якихось iнших символiв, окрiм чисел та ком.\n",
+                "\nCopied to clipboard!\n"
+            };
+            double your_position, fixed_position;
+            try
+            {
+                Console.WriteLine(str[0] + str[2]);
+                your_position = Convert.ToDouble(Console.ReadLine());
+                fixed_position = (-180 - (180 - your_position));
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(str[1] + str[2] + fixed_position);
+            }
+            catch (FormatException y)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(str[3] + y + str[4]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
